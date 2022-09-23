3643c61adc3625952a57fd649f4e1d9b378198fa
ZhenHao <zhenhao@TangChuandeMacBook-Air.local>
2022-09-23 09:20:09 +0800

    coding style 調整完成

--- app/controller/home.js
+++ app/controller/home.js
@@ -14,45 +14,39 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
 
-    const sql_query = {
+    const sqlQuery = {
       order: [[ 'id', 'desc' ]],
     };
+    const comments = await ctx.model.Comments.findAll(sqlQuery);
     
-    const comments = await ctx.model.Comments.findAll(sql_query);
-    
-    console.log(comments);
-    await this.ctx.render('home.njk', {comments: comments});
-    
+    await this.ctx.render('home.njk', { comments: comments });
   }
 
   async createPost() {
-    
     const { ctx } = this;
     const { name, message } = ctx.request.body;
+
     const result = await ctx.model.Comments.create({ name, message });
-    console.log(result);
+
     ctx.redirect('/');
   }
 
   async updatePage() {
-    
     const { ctx } = this;
     const { query } = ctx;
 
-    // const id = toInt(ctx.params.id);
     const comment = await ctx.model.Comments.findByPk(query.id);
     console.log(comment);
     if (!comment) {
       ctx.status = 404;
+
       return;
     }
 
     await this.ctx.render('update.njk', comment.dataValues);
-    
   }
 
   async updatePost() {
-    
     const { ctx } = this;
     const { request: { body } } = ctx;
 
@@ -62,6 +56,7 @@ class HomeController extends Controller {
     const comment = await ctx.model.Comments.findByPk(id);
     if (!comment) {
       ctx.status = 404;
+      
       return;
     }
     await comment.update({ name, message });
@@ -70,22 +65,16 @@ class HomeController extends Controller {
   }
 
   async deletePost() {
-    // const result = await ctx.service.home.delete(query.id);
-    // console.log(result);
-    // ctx.body = result;
-
-
     const { ctx } = this;
     const { query } = ctx;
 
-    // const id = toInt(ctx.params.id);
     const comment = await ctx.model.Comments.findByPk(query.id);
     console.log(comment);
     if (!comment) {
       ctx.status = 404;
+      
       return;
     }
-
     await comment.destroy();
     
     ctx.redirect('/');
